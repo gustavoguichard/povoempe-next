78a38478b35f77e1e082823bfe709fa96d67be7d
Gustavo Guichard <gustavoguichard@gmail.com>
2020-08-01 21:45:07 -0300

    Initial commit

---
+++ components/Layout.js
@@ -0,0 +1,7 @@
+export default ({ children }) => {
+  return (
+    <div className="antialiased text-xl text-gray-900 flex flex-col items-center justify-center border-green-700 border-b-8">
+      {children}
+    </div>
+  )
+}
