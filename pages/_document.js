3f70cd09233b9a0b664bee7d039a2ddf62080fdb
Gustavo Guichard <gustavoguichard@gmail.com>
2021-05-01 23:05:21 -0300

    Add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -6,11 +6,7 @@ class MoedaDourada extends Document {
       <Html className="max-w-screen" lang="pt-BR">
         <Head>
           <meta charSet="utf-8" />
-          <link
-            rel="shortcut icon"
-            type="image/x-icon"
-            href="/static/favicon.png"
-          />
+          <link rel="shortcut icon" type="image/x-icon" href="/favicon.png" />
         </Head>
         <body className="w-screen overflow-x-hidden antialiased text-xl text-gray-900 flex flex-col items-center justify-center border-green-700 border-b-8 pb-12">
           <Main />
