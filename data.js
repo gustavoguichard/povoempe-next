2c62d5a9c88ac10801ceaf0ace6b3747ae87c96e
Gustavo Guichard <gustavoguichard@gmail.com>
2021-05-02 14:54:49 -0300

    Add content from external markdown-like file

---
+++ data.js
@@ -0,0 +1,89 @@
+module.exports = {
+  title: 'Projeto Moeda Dourada',
+  subtitle: 'Projeto Moeda Dourada',
+  domain: 'Povo em Pé',
+  content: `Uma distribuição direta de renda à classe menos favorecida, proveniente de lucros líquidos apurados por qualquer empresa.
+
+Ao executarmos este simples e abundante projeto, pretendemos ser um farol luminoso e tocar a consciência de outros empreendedores, fomentando questionamentos sociais construtivos.
+
+E assim oportunizando a quem executar o projeto, a real possibilidade de tornar sua marca conhecida, faturar mais e consequentemente lucrar!
+
+Abaixo expomos alguns pontos importantes e convidamos você a conhecer mais a fundo nossas motivações:
+
+Existe uma enorme e crescente concentração de renda no topo da pirâmide social. Os recursos materiais e financeiros concentrados nesta ponta, são suficientes para prover todos os seres humanos com 3 refeições diárias.
+
+Entendemos ser a fome estabelecida hoje, matematicamente ilógica e humanamente bizarra.
+
+Certamente é o paradigma de necessidades infinitas em um mundo finito, reinante em uma sociedade extremamente materialista, que força a conta matemática a não fechar e ser determinante para a desumana concentração de renda.
+
+O POVO EM PÉ - CENTRO DE YOGA E AYURVEDA, quer ser um bom exemplo, para que outras empresas também usem sua liberdade e livre iniciativa para realmente socializarem seus lucros, independente de quaisquer circunstâncias, como sistema tributário vigente ou resultados baixos.
+
+Impostos ou resultados caem no paradigma citado acima.
+
+### Aqui deixamos algumas perguntas para a reflexão:
+
+- Qual a real necessidade de se acumular tanta riqueza?
+- Por que quanto mais se tem, mais se quer?
+- O acúmulo traz satisfação e felicidade reais?
+- É confortável e seguro estar no topo de uma pirâmide, mesmo com um problema ambiental que cresce na medida que a concentração de renda aumenta?
+- Ver ou saber seu semelhante sofrendo com a desigualdade e ser um acumulador de riquezas materiais, é saudável?
+
+A pandemia estabelecida no início de 2020 acentuou o debate sobre uma renda básica universal. Nós do POVO EM PÉ - CENTRO DE YOGA E AYURVEDA, entendemos que nunca houve e provavelmente nunca haverá emprego para todos.
+
+Sendo assim, decidimos não esperar que esta distribuição aconteça via iniciativas políticas e, mesmo que venha a acontecer, o que achamos imprescindível e viável, com o projeto MOEDA DOURADA, vamos demonstrar que, de uma forma simples, justa e direta, R$1,00 vai valer R$1,00.
+
+Com efeitos imediatos, pode-se acabar com a miséria e ainda aumentar as vendas e o faturamento pois, a empresa envolvida vai causar um impacto social tão grande que a exposição de sua marca, assim como o carinho dos consumidores em relação a ela, será enorme.
+
+A empresa não vai adotar um canteiro, ou uma praça, vai adotar uma família, uma rua, um bairro inteiro!
+
+### Entenda bem como funciona:
+
+Após o período de um ano e a aferição do resultado da empresa, o beneficiário escolhido, a cada 28 dias, terá o direito de sacar uma décima-terceira parte do valor total, destinado a ele ou eles, naquele ano. A operação será feita via cartão bancário, ou através de alguma organização ou instituição, nomeada para isto.
+
+Aqui uma simulação com números para exemplificar: no período de Agosto/20 até Julho/21, o POVO EM PÉ - CENTRO DE YOGA E AYURVEDA, apura um lucro líquido (receitas menos despesas) de R$ 26.000,00. Vinte por cento (20%) deste valor, R$ 5.200,00, será dividido em 13, totalizando R$ 400,00 a cada 28 dias.
+
+Este é o "Dinheiro Dourado", expressão cunhada por JOAN MELÉ.
+
+A escolha dos beneficiários terá como critério alguém que viva abaixo da linha de pobreza, que praticamente não tenha renda e que possa ser classificado como extremamente vulnerável materialmente falando.
+
+Alguns critérios específicos também serão observados e, daremos preferência às pessoas estatisticamente mais vulneráveis, como mães de crianças pequenas, que não usem drogas nem álcool, por exemplo.
+
+Sabemos que o envolvimento social é necessário e estaremos recebendo auxílio de monitores que já trabalham com classes desfavorecidas. A empatia e o calor humano, que em certos níveis são mais necessários que o dinheiro, serão importantes e fundamentais nesse processo.
+
+### E por que escolhemos o valor 20% para socializar?
+
+Firmamos 20% do lucro (independente do sistema tributário em vigor), para prover recursos que gerem uma massa crítica que realmente faça a diferença na base da pirâmide e que mexa na psique do topo.
+
+O projeto é muito simples e traz junto uma mudança de paradigma em relação ao lucro. Talvez seja um paradoxo, mas o POVO EM PÉ - CENTRO E AYURVEDA, usando de sua liberdade e também de sua livre iniciativa, está se tornando "comunista".
+
+As pessoas precisam trabalhar o desapego ao mesmo tempo que se abrem para ter uma mentalidade de abundância. Os milionários e bilionários têm essa abundância e naturalmente os mais pobres possuem mais facilidade na atitude desapegada.
+
+Socializando 20% dos lucros diretamente, estaremos trabalhando questões a nível individual e coletivo.
+
+Existem estudos que mostram que programas de transferência de renda têm a capacidade de promover o crescimento da economia.
+
+Aqui não será diferente e, o ato de compartilhar o lucro via PROJETO SOCIAL MOEDA DOURADA, será de uma forma mais intensa e principalmente amorosa.
+
+O POVO EM PÉ - CENTRO DE YOGA E AYURVEDA não está no topo da pirâmide social. O anseio é, com a repercussão de seu exemplo, atingir aos que estão neste patamar e de forma atrativa (faturar em cima com uma boa campanha publicitária) e por motivos matemáticos óbvios, beneficiar a base que sustenta o todo.
+
+Algumas quebras de paradigmas podem se resumir no entendimento de que compartilhamento não é imposto e sim um ato da livre e espontânea vontade e iniciativa.
+
+A partir de um ano da implementação deste projeto, nossa contabilidade estará à disposição dos interessados.
+
+Colocamos à disposição para maiores esclarecimentos, através do e-mail [rafapovoempe@gmail.com](mailto:rafapovoempe@gmail.com).
+
+Rafael Turra Pieruccini\\
+Fundador, guardião e administrador\\
+POVO EM PÉ - CENTRO DE YOGA E AYURVEDA
+
+Agradecemos por sua atenção\\
+Amor e União`,
+  firstBlock: `E os homens que ocupam altos cargos\\
+São os que devem começar\\
+A moldar uma nova realidade\\
+Mais próxima do coração\\
+Mais próxima do coração`,
+  lastBlock: `E se os recursos que sobem em direção ao topo voltassem para a base da pirâmide, por livre e espontânea vontade do topo?\\
+E se esse movimento aumentasse os recursos no topo, mas ao mesmo tempo aumentasse na base, onde a circulação dessa energia fosse fluida e harmônica, numa relação ganha-ganha?\\
+Teríamos então a transformação da pirâmide em um toróide, e a sociedade auto-geraria sua energia.`,
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from 'next/head'
+import Markdown from 'react-markdown'
+import data from '../data'
 
 const IndexPage = () => (
-  <>
+  <Head>
     <Head>
-      <title>Projeto Moeda Dourada | Povo em Pé</title>
+      <title>{[data.title, data.domain].join(' | ')}</title>
     </Head>
     <div className="w-screen mt-0 mb-10 flex justify-center border-green-700 border-b-8 py-12 bg-gray-200">
       <img
@@ -12,11 +14,11 @@ const IndexPage = () => (
         alt="Povo em Pé"
       />
     </div>
-    <div className="p-8 flex flex-col items-center">
+    <div className="p-8 flex flex-col items-center text-gray-600">
       <h1 className="text-center mb-10 text-green-800 font-semibold">
-        Projeto Moeda Dourada
+        {data.title}
         <br />
-        <small className="text-xl">Empresa 13:20</small>
+        <small className="text-xl">{data.subtitle}</small>
       </h1>
       <div className="max-w-lg">
         <img
@@ -28,223 +30,18 @@ const IndexPage = () => (
       <div className="max-w-3xl tracking-tight leading-snug flex flex-col items-center">
         <p className="text-gray-600 text-sm">Imagem da artista Malu Kluwe</p>
         <blockquote className="my-6">
-          E os homens que ocupam altos cargos
-          <br />
-          São os que devem começar
-          <br />
-          A moldar uma nova realidade
-          <br />
-          Mais próxima do coração
-          <br />
-          Mais próxima do coração
+          <Markdown>{data.firstBlock}</Markdown>
         </blockquote>
-        <div className="text-gray-600">
-          <p>
-            Uma distribuição direta de renda à classe menos favorecida,
-            proveniente de lucros líquidos apurados por qualquer empresa.
-          </p>
-          <p>
-            Ao executarmos este simples e abundante projeto, pretendemos ser um
-            farol luminoso e tocar a consciência de outros empreendedores,
-            fomentando questionamentos sociais construtivos.
-          </p>
-          <p>
-            E assim oportunizando a quem executar o projeto, a real
-            possibilidade de tornar sua marca conhecida, faturar mais e
-            consequentemente lucrar!
-          </p>
-          <p>
-            Abaixo expomos alguns pontos importantes e convidamos você a
-            conhecer mais a fundo nossas motivações:
-          </p>
-          <p>
-            Existe uma enorme e crescente concentração de renda no topo da
-            pirâmide social. Os recursos materiais e financeiros concentrados
-            nesta ponta, são suficientes para prover todos os seres humanos com
-            3 refeições diárias.
-          </p>
-          <p>
-            Entendemos ser a fome estabelecida hoje, matematicamente ilógica e
-            humanamente bizarra.
-          </p>
-          <p>
-            Certamente é o paradigma de necessidades infinitas em um mundo
-            finito, reinante em uma sociedade extremamente materialista, que
-            força a conta matemática a não fechar e ser determinante para a
-            desumana concentração de renda.
-          </p>
-          <p>
-            O POVO EM PÉ - CENTRO DE YOGA E AYURVEDA, quer ser um bom exemplo,
-            para que outras empresas também usem sua liberdade e livre
-            iniciativa para realmente socializarem seus lucros, independente de
-            quaisquer circunstâncias, como sistema tributário vigente ou
-            resultados baixos.
-          </p>
-          <p>Impostos ou resultados caem no paradigma citado acima.</p>
-          <h3 className="text-green-800 font-semibold tracking-tighter">
-            Aqui deixamos algumas perguntas para a reflexão:
-          </h3>
-          <ul className="list-disc">
-            <li>Qual a real necessidade de se acumular tanta riqueza?</li>
-            <li>Por que quanto mais se tem, mais se quer?</li>
-            <li>O acúmulo traz satisfação e felicidade reais?</li>
-            <li>
-              É confortável e seguro estar no topo de uma pirâmide, mesmo com um
-              problema ambiental que cresce na medida que a concentração de
-              renda aumenta?
-            </li>
-            <li>
-              Ver ou saber seu semelhante sofrendo com a desigualdade e ser um
-              acumulador de riquezas materiais, é saudável?
-            </li>
-          </ul>
-          <p>
-            A pandemia estabelecida no início de 2020 acentuou o debate sobre
-            uma renda básica universal. Nós do POVO EM PÉ - CENTRO DE YOGA E
-            AYURVEDA, entendemos que nunca houve e provavelmente nunca haverá
-            emprego para todos.
-          </p>
-          <p>
-            Sendo assim, decidimos não esperar que esta distribuição aconteça
-            via iniciativas políticas e, mesmo que venha a acontecer, o que
-            achamos imprescindível e viável, com o projeto MOEDA DOURADA, vamos
-            demonstrar que, de uma forma simples, justa e direta, R$1,00 vai
-            valer R$1,00.
-          </p>
-          <p>
-            Com efeitos imediatos, pode-se acabar com a miséria e ainda aumentar
-            as vendas e o faturamento pois, a empresa envolvida vai causar um
-            impacto social tão grande que a exposição de sua marca, assim como o
-            carinho dos consumidores em relação a ela, será enorme.
-          </p>
-          <p>
-            A empresa não vai adotar um canteiro, ou uma praça, vai adotar uma
-            família, uma rua, um bairro inteiro!
-          </p>
-          <h3 className="text-green-800 font-semibold tracking-tighter">
-            Entenda bem como funciona:
-          </h3>
-          <p>
-            Após o período de um ano e a aferição do resultado da empresa, o
-            beneficiário escolhido, a cada 28 dias, terá o direito de sacar uma
-            décima-terceira parte do valor total, destinado a ele ou eles,
-            naquele ano. A operação será feita via cartão bancário, ou através
-            de alguma organização ou instituição, nomeada para isto.
-          </p>
-          <p>
-            Aqui uma simulação com números para exemplificar: no período de
-            Agosto/20 até Julho/21, o POVO EM PÉ - CENTRO DE YOGA E AYURVEDA,
-            apura um lucro líquido (receitas menos despesas) de R$ 26.000,00.
-            Vinte por cento (20%) deste valor, R$ 5.200,00, será dividido em 13,
-            totalizando R$ 400,00 a cada 28 dias.
-          </p>
-          <p>Este é o "Dinheiro Dourado", expressão cunhada por JOAN MELÉ.</p>
-          <p>
-            A escolha dos beneficiários terá como critério alguém que viva
-            abaixo da linha de pobreza, que praticamente não tenha renda e que
-            possa ser classificado como extremamente vulnerável materialmente
-            falando.
-          </p>
-          <p>
-            Alguns critérios específicos também serão observados e, daremos
-            preferência às pessoas estatisticamente mais vulneráveis, como mães
-            de crianças pequenas, que não usem drogas nem álcool, por exemplo.
-          </p>
-          <p>
-            Sabemos que o envolvimento social é necessário e estaremos recebendo
-            auxílio de monitores que já trabalham com classes desfavorecidas. A
-            empatia e o calor humano, que em certos níveis são mais necessários
-            que o dinheiro, serão importantes e fundamentais nesse processo.
-          </p>
-          <h3 className="text-green-800 font-semibold tracking-tighter">
-            E por que escolhemos o valor 20% para socializar?
-          </h3>
-          <p>
-            Firmamos 20% do lucro (independente do sistema tributário em vigor),
-            para prover recursos que gerem uma massa crítica que realmente faça
-            a diferença na base da pirâmide e que mexa na psique do topo.
-          </p>
-          <p>
-            O projeto é muito simples e traz junto uma mudança de paradigma em
-            relação ao lucro. Talvez seja um paradoxo, mas o POVO EM PÉ - CENTRO
-            E AYURVEDA, usando de sua liberdade e também de sua livre
-            iniciativa, está se tornando "comunista".
-          </p>
-          <p>
-            As pessoas precisam trabalhar o desapego ao mesmo tempo que se abrem
-            para ter uma mentalidade de abundância. Os milionários e bilionários
-            têm essa abundância e naturalmente os mais pobres possuem mais
-            facilidade na atitude desapegada.
-          </p>
-          <p>
-            Socializando 20% dos lucros diretamente, estaremos trabalhando
-            questões a nível individual e coletivo.
-          </p>
-          <p>
-            Existem estudos que mostram que programas de transferência de renda
-            têm a capacidade de promover o crescimento da economia.
-          </p>
-          <p>
-            Aqui não será diferente e, o ato de compartilhar o lucro via PROJETO
-            SOCIAL MOEDA DOURADA, será de uma forma mais intensa e
-            principalmente amorosa.
-          </p>
-          <p>
-            O POVO EM PÉ - CENTRO DE YOGA E AYURVEDA não está no topo da
-            pirâmide social. O anseio é, com a repercussão de seu exemplo,
-            atingir aos que estão neste patamar e de forma atrativa (faturar em
-            cima com uma boa campanha publicitária) e por motivos matemáticos
-            óbvios, beneficiar a base que sustenta o todo.
-          </p>
-          <p>
-            Algumas quebras de paradigmas podem se resumir no entendimento de
-            que compartilhamento não é imposto e sim um ato da livre e
-            espontânea vontade e iniciativa.
-          </p>
-          <p>
-            A partir de um ano da implementação deste projeto, nossa
-            contabilidade estará à disposição dos interessados.
-          </p>
-          <p>
-            Colocamos à disposição para maiores esclarecimentos, através do
-            e-mail{' '}
-            <a
-              className="underline text-green-800 hover:text-green-600"
-              href="mailto:rafapovoempe@gmail.com"
-            >
-              rafapovoempe@gmail.com
-            </a>
-            .
-          </p>
-          <p>
-            Rafael Turra Pieruccini
-            <br />
-            Fundador, guardião e administrador
-            <br />
-            POVO EM PÉ - CENTRO DE YOGA E AYURVEDA
-          </p>
-          <p>
-            Agradecemos por sua atenção
-            <br />
-            Amor e União
-          </p>
-          <div className="flex flex-col gap-4 md:flex-row justify-center items-center md:items-start">
-            <img src="/mais_amor.gif" alt="Mais Amor" className="max-w-full" />
-            <blockquote className="max-w-xl italic">
-              E se os recursos que sobem em direção ao topo voltassem para a
-              base da pirâmide, por livre e espontânea vontade do topo?
-              <br />E se esse movimento aumentasse os recursos no topo, mas ao
-              mesmo tempo aumentasse na base, onde a circulação dessa energia
-              fosse fluida e harmônica, numa relação ganha-ganha?
-              <br />
-              Teríamos então a transformação da pirâmide em um toróide, e a
-              sociedade auto-geraria sua energia.
-            </blockquote>
-          </div>
+        <Markdown className="rich-text mb-8">{data.content}</Markdown>
+        <div className="flex flex-col gap-4 md:flex-row justify-center items-center md:items-start">
+          <img src="/mais_amor.gif" alt="Mais Amor" className="max-w-full" />
+          <blockquote className="max-w-xl italic">
+            <Markdown>{data.lastBlock}</Markdown>
+          </blockquote>
         </div>
       </div>
     </div>
-  </>
+  </Head>
 )
 
 export default IndexPage
